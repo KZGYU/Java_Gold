a82c5f8009eb5ae6836a75e7b0086956fc300848
KZGYU <adventcdl777@yahoo.co.jp>
2023-10-08 09:51:07 +0900

    cahpter7 スレッド内容

---
+++ src/Chapter7_PT_1Main/Main.java
@@ -0,0 +1,31 @@
+package Chapter7_PT_1Main;
+
+public class Main {
+    public static void main(String[] args) {
+        // スレッドの作成
+        Thread a = new ThreadA();
+        Thread b = new ThreadB();
+        
+        // スレッドの実行開始
+        a.start();
+        b.start();
+    }
+}
+
+// Runnableインタフェースの実装クラス
+class ThreadA extends Thread{
+    public void run() { // スレッドが実行する処理
+        for (int i = 0; i < 5; i++) {
+            System.out.print("A:" + i + " ");
+        }
+    }
+}
+
+// Runnableインタフェースの実装クラス
+class ThreadB extends Thread {
+    public void run() { // スレッドが実行する処理
+        for (int i = 0; i < 5; i++) {
+            System.out.print("B:" + i + " ");
+        }
+    }
+}
---
+++ src/Chapter7_PT_4Main/Main.java
@@ -0,0 +1,26 @@
+package Chapter7_PT_4Main;
+
+public class Main {
+	public static void main(String[] args) {
+		Thread threadA = new Thread(() -> {
+			System.out.println("threadA : sleep 開始");
+			try {
+				Thread.sleep(5000); // ThreadA スレッド の sleep
+			} catch (InterruptedException e) {
+				System.out.println("threadA : 割り込み を キャッチ し まし た");
+			}
+			System.out.println("threadA : 処理 再開");
+		});
+
+		threadA.start();
+
+		try {
+			System.out.println("main : sleep 開始");
+			Thread.sleep(2000); // main スレッド の sleep
+			System.out.println("main : sleep 終了");
+			threadA.interrupt(); // スレッド へ 割り込み
+		} catch (InterruptedException e) {
+			System.out.println("main : 割り込み を キャッチ し まし た");
+		}
+	}
+}
